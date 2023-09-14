6b2341b820d0f9804ecb0ac884a31d3209606988
Kayalvizhi-96 <144250474+Kayalvizhi-96@users.noreply.github.com>
2023-09-14 18:58:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-print('factorial of a given number')  
-i=1
-n=int(input('enter n:'))
-f=1
-while(i<=n):
-  f=f*i
-  i=i+1
-  print('factorial=',f)
+class BankAccount:
+def int  (self.account_number,account_holders_name,initial_balance==0):
+  self.__account_number=account_number
+  self.__account_holder_name=account_holder_name
+  self.__account_balance=initial_balance
+ def deposite(self.amount):
+   if amount>0:
+     self.__account_balance+=amount
+     print("deposit rs{} new balance:rs{}.format(amount,self.__account_balance"))
+   else:
+     print("invalid deposit amount.please deposit a positive amount")
+     def withdraw(self.amount):
+       if amount>0 and amount<=self.__account_balance:
+         self.__account_balance= amount
+         print("withdraw rs{} new balance:rs{}"format(amount,self.__account_balance))
+       else:
+         print("invalid withdrawal amount or insufficient balance")
+         def display_balance(self):
+           print("account balance for{}(account#{}.rs{}".format(self.__account_holder_name,self.___accound_number,self.__account_balance))
+ #create an instance of the BankAccount class
+  account=BankAccount(account_number="123456789",account_holder_name="kayal", initial_balance=2000.0)
+         
